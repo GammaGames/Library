a59ab28761b7c9a344f7e52698b9b354797ddbb2
Jesse Lieberg <jesse007@ymail.com>
2018-06-15 12:16:44 -0600

    Add linked files in index.html to cache dynamically

--- sw.js
+++ sw.js
@@ -1,31 +1,36 @@
 // Ideally you would set the version with a commit hash
 var version = new Date();
-const cacheName = `library-` + version;
-self.addEventListener('install', e => {
+cacheName = `library-` + version;
+
+// From http://craig-russell.co.uk/2016/01/29/service-worker-messaging.html#.WyPm5Z9Ki9I
+function clientPostMessage(client, message){
+    return new Promise(function(resolve, reject){
+        var channel = new MessageChannel();
+
+        channel.port1.onmessage = function(event){
+            if(event.data.error){
+                reject(event.data.error);
+            }
+            else {
+                resolve(event.data);
+            }
+        };
+        client.postMessage(message, [channel.port2]);
+    });
+}
+
+self.addEventListener('install', event => {
     console.log("installing");
     // Forces the new service worker to take over the new one after installing
     if(self.skipWaiting) {
         self.skipWaiting();
     }
     // Cache necessary files
-    e.waitUntil(
+    event.waitUntil(
         caches.open(cacheName).then(cache => {
-            console.log("caching");
             return cache.addAll([
-                // Dependencies
-                '/lib/semantic.min.css',
-                '/css/main.css',
-                '/lib/jquery.min.js',
-                '/lib/underscore.min.js',
-                '/lib/backbone.min.js',
-                '/lib/semantic.min.js',
-                // App
                 '/',
                 '/index.html',
-                '/js/models.js',
-                '/js/collections.js',
-                '/js/router.js',
-                '/js/view/views.js',
             ]).then(() => {
                 console.log("installed");
             })
@@ -36,20 +41,44 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', function(event) {
     // From https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#removing_outdated_caches
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter(function(cache) {
+                cacheNames.filter(cache => {
                     // Return true if you want to remove this cache,
                     // but remember that caches are shared across
                     // the whole origin
                     return /library/.test(cache) && cacheName != cache;
-                }).map(function(cache) {
+                }).map(cache => {
                     return caches.delete(cache);
                 })
             );
         })
     );
     console.log("old caches removed");
+
+    // Get all the clients, and for each post a message
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            // Post "addAll" to get a list of files to cache
+            clientPostMessage(client, "addAll").then(message => {
+                console.log("caching");
+                // For each file, check if it already exists in the cache
+                message.forEach(url => {
+                    caches.match(url).then(result => {
+                        // If there's nothing in the cache, fetch the file and cache it
+                        if(!result) {
+                            fetch(url).then(response => {
+                                caches.open(cacheName).then(cache => {
+                                    cache.put(url, response);
+                                });
+                            });
+                        }
+                    })
+                });
+                console.log("done");
+            });
+        })
+    });
 });
 
 self.addEventListener('fetch', function(event) {
@@ -57,9 +86,9 @@ self.addEventListener('fetch', function(event) {
     // If it is a library file, use the cache
     if(/lib/.test(event.request.url)) {
         event.respondWith(
-            caches.match(event.request).then(function(response) {
+            caches.match(event.request).then(result => {
                 // Get cache or fetch if doesn't exist
-                return result || fetch(event.request).then(function(response) {
+                return result || fetch(event.request).then(response => {
                     // Clone response, cache it, and return it
                     var clone = response.clone();
                     caches.open(cacheName).then(cache => {
@@ -74,7 +103,7 @@ self.addEventListener('fetch', function(event) {
     else {
         event.respondWith(
             // Try to fetch, if successful
-            fetch(event.request).then(function(response) {
+            fetch(event.request).then(response => {
                 // Clone response, cache it, and return it
                 var clone = response.clone();
                 caches.open(cacheName).then(cache => {
